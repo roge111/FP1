648b323b8b47195389014d630fc746796a114897
roge111 <64649951+roge111@users.noreply.github.com>
2024-11-11 18:09:40 +0300

    Временно закомментировал тесты на 25 задачу

--- ProjectTests/Tests.fs
+++ ProjectTests/Tests.fs
@@ -6,15 +6,15 @@ open projectElra25
 open projectElra6
 
 // Проверка результата вывода хвостовой рекурсивной функции
-let ``fibonachiHvost searches for the first number greater than 1000`` () =
+// let ``fibonachiHvost searches for the first number greater than 1000`` () =
 
-    let result = fibonachiHvost(1000)
-    Assert.Equal(1597, result)
+//     let result = fibonachiHvost(1000)
+//     Assert.Equal(1597, result)
 
-// Проверка результата вывода рекурсивной функцией
-let ``loop_r searches for the first number greater than 1000`` () =
-    let result  = loop_r(1000)
-    Assert.Equal(1597, result)
+// // Проверка результата вывода рекурсивной функцией
+// let ``loop_r searches for the first number greater than 1000`` () =
+//     let result  = loop_r(1000)
+//     Assert.Equal(1597, result)
 
 let ``result finds the difference between the squared sum and the sum of the squares of numbers from 1 to 100``() =
     let res = result(100)

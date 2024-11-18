a73b3de027a3b5bf1d4a8a83371f069a55e25bec
roge111 <64649951+roge111@users.noreply.github.com>
2024-11-18 18:56:39 +0300

    Замена mutable

--- projectElra25/Program.fs
+++ projectElra25/Program.fs
@@ -6,10 +6,10 @@
 let fibonachiHvost (i: int): int = 
     let rec loop (acc: int list, i)  = 
             
-            let mutable count = acc.Length
-            if acc[count-1] >= i then acc[count-1]
+            
+            if acc[acc.Length-1] >= i then acc[acc.Length-1]
             else
-                loop (List.append acc [acc[count-2] + acc[count - 1]], i)
+                loop (List.append acc [acc[acc.Length-2] + acc[acc.Length - 1]], i)
     
     loop ([1; 1], i)
 
@@ -17,9 +17,9 @@ let fibonachiHvost (i: int): int =
 
 // Реализация рекурсивной программой
 let rec loop_r(array:int list, i: int): int =
-    let mutable lastIndex = array.Length - 1
-    if array[lastIndex] >= i then array[lastIndex]
+    if array[array.Length - 1] >= i then array[array.Length - 1]
     else
-        loop_r(List.append array [array[lastIndex - 1] + array[lastIndex]], i)
+        loop_r(List.append array [array[array.Length - 2] + array[array.Length - 1]], i)
+
 
 

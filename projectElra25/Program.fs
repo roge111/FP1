ae584cd86e65aa2810788a580c4a867500c4685e
roge111 <64649951+roge111@users.noreply.github.com>
2024-11-26 15:59:24 +0300

    Добавлена реализация с генерацией чисел с map и с спец. синтаксисом для циклов

--- projectElra25/Program.fs
+++ projectElra25/Program.fs
@@ -22,4 +22,35 @@ let rec loop_r(array:int list, i: int): int =
         loop_r(List.append array [array[array.Length - 2] + array[array.Length - 1]], i)
 
 
+//Реализация при помощи List.map (отображения)
+let fibonachi (n: int): int64 = 
+    //Генерация фибоначи до i-го элемента
+    let rec fibonachi_List (array: int64 list, i: int): int64 list = 
+        if array.Length = i then array
+        else
+            fibonachi_List(List.append array [array[array.Length - 2] + array[array.Length - 1]], i)
+    //Вычисление нового элемента фибоначи
+    let f(i): int64=
+        let result:int  = 1
+        if i = 1 || i = 2 then
+            result
+        else
+            let arr :int64 list= fibonachi_List([1; 1], i)
+            arr[arr.Length - 1]
+    let rec result(n: int, array: int64 list, i: int): int64 = 
+        if array[i] >= n then array[i]
+        else
+         result(n, array, i + 1)
 
+
+    // Реализация с использование специального синтаксиса для циклов
+    let numbers: int64 list = [for i in 1..n -> i ]
+
+    let number:int64 list = [1; 1]
+    let fibolist = List.mapi (fun i x -> f(i + 1)) numbers
+
+    
+            
+
+    let res: int64 = result(n, fibolist, 0)
+    res
